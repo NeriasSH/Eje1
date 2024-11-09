69dbc29aa9a242390e86fbea741b0ea714f9e756
NeriasSH <nesh.nerias@gmail.com>
2024-11-09 15:27:12 -0500

    first commit

---
+++ src/main/java/com/ej1/eje1/Services/SpamFilterService.java
@@ -0,0 +1,19 @@
+package com.ej1.eje1.Services;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SpamFilterService {
+
+    private final List<String> spamDomains = List.of(
+            "spam.com",
+            "phishing.com",
+            "malware.com"
+            // Añade aquí más dominios de spam
+    );
+
+    public boolean isSpam(String url) {
+        return spamDomains.stream().anyMatch(url::contains);
+    }
+}
---
+++ src/test/java/com/ej1/eje1/Eje1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ej1.eje1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eje1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
